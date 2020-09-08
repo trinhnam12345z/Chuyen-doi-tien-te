65dffbf9e37d50ab5ffb03c0d981f12df1625787
trinhnam12345z <trinhnam123456789z@gmail.com>
2020-09-08 20:14:27 +0700

    Ứng dụng chuyển đổi tiền tệ

---
+++ Chuyen doi tien te/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Chuyen_doi_tien_te
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("CT CHUYỂN ĐỔI TIỀN TỆ USD -> VNĐ");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Nhập tỉ giá tiền (Ví dụ 23000 Vnđ/USD):");
+            int tigia = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập USD :");
+            int usd = int.Parse(Console.ReadLine());
+            int vnd = tigia * usd ;
+            Console.WriteLine("VNĐ là : {0} Vnđ",vnd);
+        }
+    }
+}
